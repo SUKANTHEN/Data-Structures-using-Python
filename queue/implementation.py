6b53763e3e0d324c11a187fa2de3c902e5493bcf
SUKANTHEN.S.S <49028941+SUKANTHEN@users.noreply.github.com>
2022-03-14 12:37:28 +0530

    using lists

--- queue/implementation.py
+++ queue/implementation.py
@@ -1,1 +1,20 @@
+queue = [] 
+# Adding elements to the queue
+queue.append('s')
+queue.append('u')
+queue.append('k')
+queue.append('a')
+queue.append('n')
 
+print("After Inserting into queue or Enqueue:")
+print(queue)
+ 
+# Removing elements from the queue as per FIFO Principle
+print("\nElements dequeued from queue:")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing elements")
+print(queue)
